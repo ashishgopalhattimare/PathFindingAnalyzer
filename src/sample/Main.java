95a86dfbf7fd4de8771ddb4ba98da615d1ce71f3
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2019-12-26 19:44:00 +0530

    Project Directory refactored and Github name modified and checked after this and finally pushed again to save latest update to the project

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
